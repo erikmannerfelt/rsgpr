6b7ba8bccf533648cf9e4f97ab19a7f5d33639d6
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2022-12-03 23:40:05 +0100

    Successfully compiled on nixos unstable

--- flake.nix
+++ flake.nix
@@ -1,14 +1,22 @@
 {
   description = "rsgpr development shell";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # 22.05 is used for GDAL 3.5 (see more in 'shell.nix')
+    nixpkgs-2205.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   
-  outputs = {self, nixpkgs, flake-utils}: 
+  outputs = {self, nixpkgs, flake-utils, nixpkgs-2205}: 
     flake-utils.lib.eachDefaultSystem 
       (system: 
-        let pkgs = nixpkgs.legacyPackages.${system}; in 
+        let 
+          pkgs = nixpkgs.legacyPackages.${system}; 
+          old-pkgs = nixpkgs-2205.legacyPackages.${system};
+        in 
         {
-          devShells.default = import ./shell.nix {inherit pkgs;};
+          devShells.default = import ./shell.nix {inherit pkgs; gdal=old-pkgs.gdal;};
         }
       
       );
--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, gdal ? null }:
+
+let 
+  # The rust gdal system is a bit hard to compile without precompiled headers.
+  # So the minimum supported gdal version is currently 3.5 (2022-11-03)
+  my-gdal = if gdal != null then gdal else pkgs.gdal;
+
+in
 
 pkgs.mkShell {
 
@@ -13,11 +20,12 @@ pkgs.mkShell {
        proj.dev
        libclang
        clang
-       gdal
+       my-gdal
+
   ];
   LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
   PKG_CONFIG_PATH="${pkgs.gdal}/lib/pkgconfig/:${pkgs.proj.dev}/lib/pkgconfig";
-  BINDGEN_EXTRA_CLANG_ARGS="-I ${pkgs.proj.dev}/include -I ${pkgs.clang}/resource-root/include";
+  BINDGEN_EXTRA_CLANG_ARGS="-I ${pkgs.proj.dev}/include -I ${pkgs.clang}/resource-root/include -I ${pkgs.gcc}/include";
 
   shellHook = ''
       ${pkgs.zsh}/bin/zsh
