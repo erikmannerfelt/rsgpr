e53c5aa2d617645d618a50f9742e689bf4f99e40
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2023-06-06 12:52:05 +0200

    Added default.nix to build the package and added default package in the flake to call default.nix

--- flake.nix
+++ flake.nix
@@ -5,16 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  
-  outputs = {self, nixpkgs, flake-utils}: 
-    flake-utils.lib.eachDefaultSystem 
-      (system: 
-        let 
-          pkgs = nixpkgs.legacyPackages.${system}; 
-        in 
-        {
-          devShells.default = import ./shell.nix {inherit pkgs;};
-        }
-      
-      );
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = import ./shell.nix { inherit pkgs; };
+        packages = rec {
+          rsgpr = import ./default.nix { inherit pkgs; };
+          default = rsgpr;
+        };
+      }
+
+    );
 }
