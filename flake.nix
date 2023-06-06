fc7d270b8c043251c0bfce62070c1e35f0ef30b4
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2023-06-06 12:24:50 +0200

    Updated gdal-sys, flake and removed legacy GDAL flake hack.

--- flake.nix
+++ flake.nix
@@ -3,20 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # 22.05 is used for GDAL 3.5 (see more in 'shell.nix')
-    nixpkgs-2205.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   
-  outputs = {self, nixpkgs, flake-utils, nixpkgs-2205}: 
+  outputs = {self, nixpkgs, flake-utils}: 
     flake-utils.lib.eachDefaultSystem 
       (system: 
         let 
           pkgs = nixpkgs.legacyPackages.${system}; 
-          old-pkgs = nixpkgs-2205.legacyPackages.${system};
         in 
         {
-          devShells.default = import ./shell.nix {inherit pkgs; gdal=old-pkgs.gdal;};
+          devShells.default = import ./shell.nix {inherit pkgs;};
         }
       
       );
