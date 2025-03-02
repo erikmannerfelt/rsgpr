e951439878ed1aeb37c5d4cf615a52b75b810f1a
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2025-03-02 18:11:34 +0100

    Removed old compilation flags and updated version to 0.2.1

--- shell.nix
+++ shell.nix
@@ -15,9 +15,6 @@ pkgs.mkShell {
     proj
     gdal
   ];
-  LIBCLANG_PATH = package.LIBCLANG_PATH;
-  PKG_CONFIG_PATH = package.PKG_CONFIG_PATH;
-  BINDGEN_EXTRA_CLANG_ARGS = package.BINDGEN_EXTRA_CLANG_ARGS;
 
   shellHook = ''
     ${pkgs.zsh}/bin/zsh
