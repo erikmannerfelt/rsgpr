f4dabb494c98b0521f33fe320ad0155607769829
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2025-03-04 12:30:01 +0100

    Added support for pre-GDAL 3.10 sampling with a warning that it's nearest neighbor.

--- src/dem.rs
+++ src/dem.rs
@@ -3,6 +3,53 @@ use std::path::Path;
 
 use crate::coords::Coord;
 
+fn get_gdal_version() -> Result<String, String> {
+    let child = std::process::Command::new("gdal-config")
+        .arg("--version")
+        .stderr(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("Call error when spawning process: {e}"))?;
+
+    let output = child
+        .wait_with_output()
+        .map_err(|e| format!("Call failed: {e}"))?;
+
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).trim().to_string())
+    } else {
+        if output.stderr.is_empty() {
+            Err("Unknown error getting GDAL version.".to_string())
+        } else {
+            Err(format!(
+                "Error getting GDAL version: {}",
+                String::from_utf8_lossy(&output.stderr)
+            ))
+        }
+    }
+}
+
+fn supports_interpolation() -> Result<bool, String> {
+    let version = get_gdal_version()?;
+
+    let mut parts = version.split(".");
+    let error_msg = format!("Unrecognized version format: {version}");
+
+    if let Some(major) = parts.next() {
+        if major.parse::<usize>().map_err(|_| error_msg.clone())? < 3 {
+            return Ok(false);
+        }
+    }
+
+    if let Some(minor) = parts.next() {
+        let minor = minor.parse::<usize>().map_err(|_| error_msg.clone())?;
+
+        return Ok(minor >= 10);
+    }
+
+    return Err(error_msg);
+}
+
 pub fn sample_dem(dem_path: &Path, coords_wgs84: &Vec<Coord>) -> Result<Vec<f32>, String> {
     use std::io::Write;
 
@@ -10,15 +57,19 @@ pub fn sample_dem(dem_path: &Path, coords_wgs84: &Vec<Coord>) -> Result<Vec<f32>
         return Err("Coords vec is empty.".into());
     }
 
-    let args = vec![
+    let mut args = vec![
         "-xml",
         "-b",
         "1",
         "-wgs84",
-        // "-r",
-        // "bilinear",
         dem_path.to_str().ok_or("Empty DEM path given")?,
     ];
+    if supports_interpolation()? {
+        args.push("-r");
+        args.push("bilinear");
+    } else {
+        eprintln!("GDAL version lower than 3.10. Falling back on nearest neighbor sampling.");
+    }
     let mut child = std::process::Command::new("gdallocationinfo")
         .args(args)
         .stdin(std::process::Stdio::piped())
@@ -135,6 +186,12 @@ mod tests {
             let result = super::sample_dem(dem_path, &coord_wgs84);
 
             if let Ok(expected_elevation) = expected {
+                if !super::supports_interpolation().unwrap() {
+                    eprintln!(
+                        "Skipping elevation Ok assertion because interpolation is not supported."
+                    );
+                    continue;
+                }
                 assert_eq!(Ok(vec![expected_elevation]), result);
             } else if let Err(expected_err_str) = expected {
                 if let Err(err_str) = result {
